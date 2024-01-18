ce47701125450e07564440302f0b00d0c06aca1d
Neenu025 <neenujose025@gmail.com>
2024-01-18 22:54:35 +0530

    first commit

---
+++ Practical1.js
@@ -0,0 +1,49 @@
+var age = 25;
+console.log(age);
+alert("Hello")
+let Name = "Bincy"
+Name = "Jose"
+console.log(Name)
+
+const letter = "box"
+console.log(letter)
+
+
+let value = 15;
+value-=20;
+
+console.log(value)
+
+alert(prompt("Enter your name"))
+
+let Designation = prompt("What is your designation");
+alert(Designation);
+
+let Namee = "Wonderland"
+console.log(`Welcome to ${Namee}`)
+
+
+
+// JS conditional statements Example 1 //
+
+if(10>5){
+    console.log("Big")
+}else{
+    console.log("Small")
+}
+
+// JS conditional statements Example 2 //
+
+let num1 = 10
+let num2 = 5
+
+if(num1>num2){
+    console.log("Big")
+}else if(num1==num2){
+    console.log("Same")
+}else{
+    console.log("Small")
+}
+
+
+
