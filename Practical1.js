ebba745ef7aefbb4c00a0be12a89d5d3e244f4ea
Neenu025 <neenujose025@gmail.com>
2024-01-19 09:19:53 +0530

    Switch statement

--- Practical1.js
+++ Practical1.js
@@ -1,49 +1,64 @@
 var age = 25;
 console.log(age);
-alert("Hello")
-let Name = "Bincy"
-Name = "Jose"
-console.log(Name)
+alert("Hello");
+let Name = "Bincy";
+Name = "Jose";
+console.log(Name);
 
-const letter = "box"
-console.log(letter)
-
+const letter = "box";
+console.log(letter);
 
 let value = 15;
-value-=20;
+value -= 20;
 
-console.log(value)
+console.log(value);
 
-alert(prompt("Enter your name"))
+alert(prompt("Enter your name"));
 
 let Designation = prompt("What is your designation");
 alert(Designation);
 
-let Namee = "Wonderland"
-console.log(`Welcome to ${Namee}`)
-
-
+let Namee = "Wonderland";
+console.log(`Welcome to ${Namee}`);
 
 // JS conditional statements Example 1 //
 
-if(10>5){
-    console.log("Big")
-}else{
-    console.log("Small")
+if (10 > 5) {
+  console.log("Big");
+} else {
+  console.log("Small");
 }
 
 // JS conditional statements Example 2 //
 
-let num1 = 10
-let num2 = 5
+let num1 = 10;
+let num2 = 5;
 
-if(num1>num2){
-    console.log("Big")
-}else if(num1==num2){
-    console.log("Same")
-}else{
-    console.log("Small")
+if (num1 > num2) {
+  console.log("Big");
+} else if (num1 == num2) {
+  console.log("Same");
+} else {
+  console.log("Small");
 }
 
+//switch statement //
 
-
+let number = prompt("Enter a number between 1 - 4");
+switch (number) {
+  case "1":
+    alert("One");
+    break;
+  case "2":
+    alert("Two");
+    break;
+  case "3":
+    alert("Three");
+    break;
+  case "4":
+    alert("Four");
+    break;
+  default:
+    alert("Not found");
+}
+//Switch statement end
