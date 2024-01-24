b99ad7b0fbb96d0cb419d940b52190e7c37c7027
Neenu025 <neenujose025@gmail.com>
2024-01-24 12:59:08 +0530

    async_functions

---
+++ Promise.race.js
@@ -0,0 +1,16 @@
+const p1 = new Promise((resolve,reject) =>{
+    setTimeout(()=> resolve("P1 success"),3000)
+})
+const p2 = new Promise((resolve,reject) =>{
+    // setTimeout(()=> resolve("P2 success"),1000)
+    setTimeout(()=> reject("P2 fail"),1000)
+})
+const p3 = new Promise((resolve,reject) =>{
+    setTimeout(()=> resolve("P3 success"),2000)
+})
+Promise.race([p1,p2,p3]).then(res =>{
+    console.log(res)
+})
+.catch((err)=>{
+    console.error(err)
+})
