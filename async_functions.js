c7f7f18a0e73ca48e5c52ad49ebd6da496278518
Neenu025 <neenujose025@gmail.com>
2024-01-24 16:42:13 +0530

    changes

--- async_functions.js
+++ async_functions.js
@@ -1,5 +1,22 @@
+//a new promise
+const p = new Promise((resolve, reject)=> {
+    resolve("Promise resolved value")
+})
+
+
+
+//async function
 async function getData(){
-    return "Namaste"
-}const dataPromise = getData()
+    return p
+}
+const dataPromise = getData()
 
 dataPromise.then (res => console.log(res))
+
+
+//async await example
+async function handlePromises(){
+    const val = await p
+    console.log(val)
+}
+handlePromises()
